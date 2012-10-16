81a58c7e1d4e5f430903890cc33445f2b79269a7
avianey <antoine.vianey@gmail.com>
2012-10-16 16:27:37 +0200

    Support f**ck**g formatting of pref summary since Gingerbread

--- Level/src/net/androgames/level/LevelPreferences.java
+++ Level/src/net/androgames/level/LevelPreferences.java
@@ -10,6 +10,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
@@ -92,8 +93,7 @@ public class LevelPreferences extends PreferenceActivity implements OnPreference
     	findPreference(KEY_VISCOSITY).setOnPreferenceChangeListener(this);
     	findPreference(KEY_ECONOMY).setOnPreferenceChangeListener(this);
     	// mise a jour de l'affichage
-    	findPreference(KEY_DISPLAY_TYPE).setSummary(DisplayType.valueOf(
-    			prefs.getString(LevelPreferences.KEY_DISPLAY_TYPE, "ANGLE")).getSummary()); 
+    	onPreferenceChange(findPreference(KEY_DISPLAY_TYPE), prefs.getString(LevelPreferences.KEY_DISPLAY_TYPE, "ANGLE")); 
     	findPreference(KEY_SENSOR).setSummary(Provider.valueOf(
     			prefs.getString(LevelPreferences.KEY_SENSOR, PROVIDER_ACCELEROMETER)).getSummary());
     	findPreference(KEY_VISCOSITY).setSummary(Viscosity.valueOf(
@@ -131,7 +131,12 @@ public class LevelPreferences extends PreferenceActivity implements OnPreference
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
 		String key = preference.getKey();
 		if (KEY_DISPLAY_TYPE.equals(key)) {
-			preference.setSummary(DisplayType.valueOf((String) newValue).getSummary());
+		    CharSequence displaySummary = getText(DisplayType.valueOf((String) newValue).getSummary());
+		    if (Build.VERSION.SDK_INT >= 11 /* 3.0 : HoneyComb */) {
+		        // Fucking retro-compatibility !!!
+		        displaySummary = String.valueOf(displaySummary).replaceAll("%", "%%");
+		    }
+			preference.setSummary(displaySummary);
 		} else if (KEY_SENSOR.equals(key)) {
 			preference.setSummary(Provider.valueOf((String) newValue).getSummary());
 	    	showDialog(DIALOG_CALIBRATE_AGAIN);
